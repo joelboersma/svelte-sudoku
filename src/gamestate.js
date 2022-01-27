3446c4ba12c328df8adc390724b96e55e7078345
Joel Boersma <joelboersma@gmail.com>
2022-01-26 18:29:16 -0800

    fill diagonal Subtables

--- src/gamestate.js
+++ src/gamestate.js
@@ -1,5 +1,9 @@
 import { writable } from "svelte/store";
 
+const randomArrayElem = (arr) => {
+   return arr[Math.floor(Math.random() * arr.length)];
+}
+
 const EMPTY_TABLE = (() => {
    let table = [];
    for (let i = 0; i < 9; i++) {
@@ -17,25 +21,28 @@ const EMPTY_TABLE = (() => {
    return table;
 })();
 
-const generateTable = () => {
-   
-   let table = [];
-
-   for (let i = 0; i < 9; i++) {
-      let subTable = [];
-      for (let j = 0; j < 9; j++) {
-         const isStatic = Math.round(Math.random()) == 0;
-         subTable.push({
-            static: isStatic,
-            subTable: i,
-            pos: j,
-            value: isStatic ? Math.round(Math.random() * 8 + 1) : undefined
-         });
+const fillDiagonals = (table) => {
+   const SUB_TABLES_TO_FILL = [0, 4, 8];
+   for (let subTableIndex of SUB_TABLES_TO_FILL) {
+      let subTable = table[subTableIndex];
+      let vals = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      for (let i = 0; i < subTable.length; i++) {
+         const val = randomArrayElem(vals);
+         subTable[i].value = val;
+         vals = vals.filter(n => n != val);
       }
-      table.push(subTable);
+      table[subTableIndex] = subTable;
    }
-
    return table;
+}
+
+// const fillDiagonals = (table) => {
+//    return table;
+// }
+
+const generateTable = () => {
+   let diagonalFilledTable = fillDiagonals(EMPTY_TABLE);
+   return diagonalFilledTable;
 }
 
 export const GamestateStore = writable({
