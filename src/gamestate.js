fbb5aa0b43d2afc0b4ec0297ca890bada4318535
Joel Boersma <joelboersma@gmail.com>
2022-01-26 17:53:39 -0800

    generate table filled with 0's

--- src/gamestate.js
+++ src/gamestate.js
@@ -1,5 +1,22 @@
 import { writable } from "svelte/store";
 
+const EMPTY_TABLE = (() => {
+   let table = [];
+   for (let i = 0; i < 9; i++) {
+      let subTable = [];
+      for (let j = 0; j < 9; j++) {
+         subTable.push({
+            static: false,
+            subTable: i,
+            pos: j,
+            value: 0
+         });
+      }
+      table.push(subTable);
+   }
+   return table;
+})();
+
 const generateTable = () => {
    
    let table = [];
